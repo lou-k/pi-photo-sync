b4991663191f0c71f31a5465ef1d1b995c9700c7
Alan Hoyle <alan@alanhoyle.com>
2024-12-27 18:37:14 -0500

    Friendlier install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,21 @@ set -euo pipefail
 IFS=$'\n\t'
 
 apt-get install -y exiftool at
-cp config/systemd/* /etc/systemd/system/
-cp src/* /usr/local/bin/
-cp destinations/ssh-copy-and-organize/piphoto-ssh-sync /usr/local/bin/
-cp destinations/dropbox/piphoto-dropbox-sync /usr/local/bin/
-cp config/udev/* /etc/udev/rules.d/
+
+set +x
+echo "Copying scripts into system locations..."
+set -x
+
+cp -av config/systemd/* /etc/systemd/system/
+cp -av src/* /usr/local/bin/
+cp -av destinations/ssh-copy-and-organize/piphoto-ssh-sync /usr/local/bin/
+cp -av destinations/dropbox/piphoto-dropbox-sync /usr/local/bin/
+cp -av config/udev/* /etc/udev/rules.d/
+
 udevadm control --reload
 systemctl daemon-reload
 systemctl enable piphoto.service
-echo "Executables and udev rules installed. You'll need to create /etc/piphoto.conf if you haven't already"
+
+set +x
+echo "Executables and udev rules installed."
+echo "    You'll need to create /etc/piphoto.conf if you haven't already"
